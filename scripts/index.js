ad0732c3d11c8163a744aa38928b1b823a05dbd2
Karina Danielian <danieliankv@gmail.com>
2023-10-20 18:33:46 +0300

    первая версия проекта

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+function addCard(element, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = element.name;
+  cardElement.querySelector('.card__title').textContent = element.name;
+
+  deleteButton.addEventListener('click', deleteCard);
+
+  return cardElement;
+};
+
+function deleteCard(evt) {
+  evt.target.closest('.places__item').remove();
+}
+
+initialCards.forEach(function (element) {
+  const card = addCard(element, deleteCard);
+  placesList.append(card);
+});
+
+
+
