63d3120f5909ec878bab2dc24e83cf41f0cd5390
Karina Danielian <danieliankv@gmail.com>
2023-10-23 14:17:15 +0300

    внесены правки после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -11,25 +11,27 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
-function addCard(element, deleteCard) {
+function createCard(element, deleteCard) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   const deleteButton = cardElement.querySelector('.card__delete-button');
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
   
-  cardElement.querySelector('.card__image').src = element.link;
-  cardElement.querySelector('.card__image').alt = element.name;
-  cardElement.querySelector('.card__title').textContent = element.name;
+  cardImage.src = element.link;
+  cardImage.alt = element.name;
+  cardTitle.textContent = element.name;
 
   deleteButton.addEventListener('click', deleteCard);
 
   return cardElement;
-};
+}
 
 function deleteCard(evt) {
   evt.target.closest('.places__item').remove();
 }
 
 initialCards.forEach(function (element) {
-  const card = addCard(element, deleteCard);
+  const card = createCard(element, deleteCard);
   placesList.append(card);
 });
 
